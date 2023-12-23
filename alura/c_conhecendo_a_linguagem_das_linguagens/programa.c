45e6502fc4fd49e4b47134c5139ee4fa197466f2
Gilberto Ribeiro Pinto Júnior <gilbertoribeirocvt@gmail.com>
2023-12-23 03:42:23 -0300

    Aplicando condicionais

--- alura/c_conhecendo_a_linguagem_das_linguagens/programa.c
+++ alura/c_conhecendo_a_linguagem_das_linguagens/programa.c
@@ -1,13 +1,34 @@
 #include <stdio.h>
 
 int main() {
-    // Início do cabeçalho
+
     int numero_secreto, chute;
     numero_secreto = 42;
+
+    // Início do cabeçalho
     printf("******************************************\n");
     printf("* Bem-vindo ao nosso jogo de adivinhação *\n");
     printf("******************************************\n");
+
     printf("Insira o seu chute: ");
     scanf("%d", &chute);
-    printf("Seu chute foi: %d\n", chute);
+
+    int acertou = (chute == numero_secreto);
+
+    printf("Código acertou: %d.\n", acertou);
+
+    if(acertou) {
+        printf("Parabéns! Você acertou!\n");
+    }
+    else {
+
+        int maior = (chute > numero_secreto);
+
+        if(maior) {
+            printf("O seu chute foi maior que o número secreto\n");
+        }
+        else {
+            printf("O seu chute foi menor que o número secreto.\n");
+        }
+    }
 }
