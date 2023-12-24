05c1bce1ad411c8113b1c7ff6bf99543c29837c9
Gilberto Ribeiro Pinto Júnior <gilbertoribeirocvt@gmail.com>
2023-12-24 01:58:48 -0300

    Implementação da função rand()

--- alura/c_conhecendo_a_linguagem_das_linguagens/programa.c
+++ alura/c_conhecendo_a_linguagem_das_linguagens/programa.c
@@ -1,14 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 #define PONTUACAO_MAXIMA 1000
+#define INTERVALO 100
 
 int main() {
 
-    int numero_secreto, chute, tentativas, pontos, pontos_perdidos;
-    pontos = PONTUACAO_MAXIMA;
+    int segundo = time(0);
+    srand(segundo);
+
+    int numero_secreto, chute, tentativas;
+    double pontos = PONTUACAO_MAXIMA;
     tentativas = 1;
-    numero_secreto = 42;
+    numero_secreto = rand() % INTERVALO + 1;
 
     // Início do cabeçalho
     printf("******************************************\n");
@@ -38,11 +43,24 @@ int main() {
             printf("O seu chute foi menor que o número secreto.\n");
         }
         tentativas++;
-        pontos_perdidos = abs(chute - numero_secreto) / 2;
+        // Casting:
+        double pontos_perdidos = (double)abs(chute - numero_secreto) / (double)2;
         pontos -= pontos_perdidos;
     }
 
     printf("Fim de jogo!\n");
     printf("Você acertou em %d tentativas!\n", tentativas);
-    printf("Pontuação final: %d.\n", pontos);
+    printf("Pontuação final: %.1f.\n", pontos);
 }
+
+/*
+Números inteiros:
+short: 2 bytes - 16 bits
+int: 4 bytes - 32 bits
+long: 8 bytes - 64 bits
+
+Números flutuantes:
+float: 4 bytes - 32 bits
+double: 8 bytes - 64 bits
+*/
+
