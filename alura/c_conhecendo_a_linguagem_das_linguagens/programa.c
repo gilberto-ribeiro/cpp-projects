daf8e5ce41d8183de61ea1a1ee9c8400529d73fb
Gilberto Ribeiro Pinto Júnior <gilbertoribeirocvt@gmail.com>
2023-12-24 00:01:32 -0300

    Pontuação

--- alura/c_conhecendo_a_linguagem_das_linguagens/programa.c
+++ alura/c_conhecendo_a_linguagem_das_linguagens/programa.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+#define PONTUACAO_MAXIMA 1000
 
 int main() {
 
-    int numero_secreto, chute, tentativas;
+    int numero_secreto, chute, tentativas, pontos, pontos_perdidos;
+    pontos = PONTUACAO_MAXIMA;
     tentativas = 1;
     numero_secreto = 42;
 
@@ -28,14 +32,17 @@ int main() {
             break;
         }
         else if(maior) {
-            printf("O seu chute foi maior que o número secreto\n");
+            printf("O seu chute foi maior que o número secreto.\n");
         }
         else {
             printf("O seu chute foi menor que o número secreto.\n");
         }
         tentativas++;
+        pontos_perdidos = abs(chute - numero_secreto) / 2;
+        pontos -= pontos_perdidos;
     }
 
     printf("Fim de jogo!\n");
     printf("Você acertou em %d tentativas!\n", tentativas);
+    printf("Pontuação final: %d.\n", pontos);
 }
