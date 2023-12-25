e3a35cce3d0f79ac23e3f037499089c3b6f2ada4
Gilberto Ribeiro Pinto Júnior <gilbertoribeirocvt@gmail.com>
2023-12-25 02:13:10 -0300

    Início do jogo de forca (arrays, chars e strings)

---
+++ alura/c_avancando_na_linguagem/forca.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+
+    char palavra[20];
+
+    sprintf(palavra, "MELANCIA");
+
+    printf("%s\n", palavra);
+
+    int acertou = 0;
+    int enforcou = 0;
+
+/*
+    do {
+    // COMPLETAR
+    } while(!acertou && !enforcou)
+*/
+}
