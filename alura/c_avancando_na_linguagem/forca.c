79977b9fd4f620fe23e1ea8dfa45a273e2e153ac
Gilberto Ribeiro Pinto Júnior <gilbertoribeirocvt@gmail.com>
2023-12-26 01:49:26 -0300

    Criação do header file.

--- alura/c_avancando_na_linguagem/forca.c
+++ alura/c_avancando_na_linguagem/forca.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "forca.h"
 
 // Variáveis globais
 char palavra_secreta[20];
---
+++ alura/c_avancando_na_linguagem/forca.h
@@ -0,0 +1,7 @@
+void abertura();
+void chuta();
+int ja_chutou(char letra);
+void desenha_forca();
+void escolhe_palavra();
+int acertou();
+int enforcou();
