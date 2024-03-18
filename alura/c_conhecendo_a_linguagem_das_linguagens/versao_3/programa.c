1f4daab0d21a5796e2d6431fd81f2f117aec85fa
Gilberto Ribeiro Pinto Junior <gilbertoribeirocvt@gmail.com>
2024-03-18 18:02:31 +0000

    Primeira versão com cabeçalho

---
+++ alura/c_conhecendo_a_linguagem_das_linguagens/versao_3/programa.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() {
+    // Imprime o cabeçalho do nosso jogo
+    printf("************************************\n");
+    printf("* Bem vindo ao jogo de adivinhação *\n");
+    printf("************************************\n");
+}
