dfa51e81404833d3b86ee2517ddfd5babf728e3d
Gilberto Ribeiro Pinto Junior <gilbertoribeirocvt@gmail.com>
2024-03-19 11:28:44 +0000

    Desafio da Multiplicação

---
+++ alura/c_conhecendo_a_linguagem_das_linguagens/versao_3/desafio_1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    int x, y;
+    printf("Digite o valor de x: ");
+    scanf("%d", &x);
+    printf("Digite o valor de y: ");
+    scanf("%d", &y);
+    printf("A multiplicação de x e y resulta em %d.\n", x*y);
+}
